1dbd9c11529064ae463e29dcaa03dc6256729a6d
Bridget Williams <bridget_williams1@unc.edu>
2019-03-24 23:57:09 -0400

    Finished remove

--- bulletin-board/src/board.js
+++ bulletin-board/src/board.js
@@ -21,6 +21,7 @@ class Board extends Component {
 
     this.eachNote = this.eachNote.bind(this);
     this.update = this.update.bind(this);
+    this.remove = this.remove.bind(this);
   }
 
   update(newText, i) {
@@ -30,9 +31,16 @@ class Board extends Component {
       ),
     }));
   }
+
+  remove(id) {
+    this.setState((prevState) => ({
+      notes: prevState.notes.filter((note) => note.id !== id),
+    }));
+  }
+
   eachNote(note, i) {
     return (
-      <Note key={i} index={i} onChange={this.update} > {note.note}</Note>
+      <Note key={i} index={i} onChange={this.update} onRemove={this.remove} > {note.note}</Note>
     );
   }
 
